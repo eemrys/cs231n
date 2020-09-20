257514aff3b63cfbfc8c6de9a889d463fd17262c
eemrys <49338231+eemrys@users.noreply.github.com>
2020-09-20 21:28:23 +0300

    assignment upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# CS231n: Convolutional Neural Networks for Visual Recognition -- Assignment Solutions
+# Assignment 1: Image Classification, kNN, SVM, Softmax, Two-Layer Neural Network
+
+## Goals
+
+In this assignment you will practice putting together a simple image classification pipeline based on the k-Nearest Neighbor or the SVM/Softmax classifier. The goals of this assignment are as follows:
+
+* Understand the basic Image Classification pipeline and the data-driven approach (train/predict stages)
+* Understand the train/val/test splits and the use of validation data for hyperparameter tuning.
+* Develop proficiency in writing efficient vectorized code with numpy
+* Implement and apply a k-Nearest Neighbor (kNN) classifier
+* Implement and apply a Multiclass Support Vector Machine (SVM) classifier
+* Implement and apply a Softmax classifier
+* Implement and apply a Two layer neural network classifier
+* Understand the differences and tradeoffs between these classifiers
+* Get a basic understanding of performance improvements from using higher-level representations as opposed to raw pixels, e.g. color histograms, Histogram of Gradient (HOG) features, etc.
+
+## Q1: k-Nearest Neighbor classifier
+
+The notebook [`knn.ipynb`](https://gitlab.com/labsallday/interns/cs231n_alina/-/blob/assignment-1/assignment1/knn.md) will walk you through implementing the kNN classifier.
+
+## Q2: Training a Support Vector Machine
+
+The notebook [`svm.ipynb`](https://gitlab.com/labsallday/interns/cs231n_alina/-/blob/assignment-1/assignment1/svm.md) will walk you through implementing the SVM classifier.
+
+## Q3: Implement a Softmax classifier
+
+The notebook [`softmax.ipynb`](https://gitlab.com/labsallday/interns/cs231n_alina/-/blob/assignment-1/assignment1/softmax.md) will walk you through implementing the Softmax classifier.
+
+## Q4: Two-Layer Neural Network
+
+The notebook [`two_layer_net.ipynb`](https://gitlab.com/labsallday/interns/cs231n_alina/-/blob/assignment-1/assignment1/two_layer_net.md) will walk you through the implementation of a two-layer neural network classifier.
+
+## Q5: Higher Level Representations: Image Features
+
+The notebook [`features.ipynb`](https://gitlab.com/labsallday/interns/cs231n_alina/-/blob/assignment-1/assignment1/features.md) will examine the improvements gained by using higher-level representations as opposed to using raw pixel values.
+
---
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -0,0 +1,133 @@
+from builtins import range
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+
+def svm_loss_naive(W, X, y, reg):
+    """
+    Structured SVM loss function, naive implementation (with loops).
+
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
+
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    dW = np.zeros(W.shape) # initialize the gradient as zero
+
+    # compute the loss and the gradient
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    loss = 0.0
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        correct_class_score = scores[y[i]]
+        for j in range(num_classes):
+            if j == y[i]:
+                continue
+            margin = scores[j] - correct_class_score + 1 # note delta = 1
+            if margin > 0:
+                loss += margin
+                dW[:, y[i]] -= X[i, :]  # Correct class row
+                dW[:, j] += X[i, :]    # Incorrect class row
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    dW /= num_train
+
+    # Add regularization to the loss.
+    loss += reg * np.sum(W * W)
+    dW += 2 * reg * W
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Compute the gradient of the loss function and store it dW.                #
+    # Rather than first computing the loss and then computing the derivative,   #
+    # it may be simpler to compute the derivative at the same time that the     #
+    # loss is being computed. As a result you may need to modify some of the    #
+    # code above to compute the gradient.                                       #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    """
+    ...
+    
+    dW[:, y[i]] -= X[i, :]  # Correct class row
+    dW[:, j] += X[i, :]    # Incorrect class row
+    
+    ...
+    
+    dW /= num_train
+    
+    ...
+    
+    dW += 2 * reg * W
+
+    """
+
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    
+    return loss, dW
+
+
+
+def svm_loss_vectorized(W, X, y, reg):
+    """
+    Structured SVM loss function, vectorized implementation.
+
+    Inputs and outputs are the same as svm_loss_naive.
+    """
+    loss = 0.0
+    dW = np.zeros(W.shape) # initialize the gradient as zero
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the structured SVM loss, storing the    #
+    # result in loss.                                                           #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    num_train = X.shape[0]
+    
+    scores = X.dot(W)
+    yi_scores = scores[np.arange(num_train),y]
+    
+    margins = np.maximum(0, scores - np.matrix(yi_scores).T + 1)
+    margins[np.arange(num_train),y] = 0
+    
+    loss = np.mean(np.sum(margins, axis=1)) # np.mean is equivalent to dividing by num_train
+    loss += reg * np.sum(W * W)
+
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    #############################################################################
+    # TODO:                                                                     #
+    # Implement a vectorized version of the gradient for the structured SVM     #
+    # loss, storing the result in dW.                                           #
+    #                                                                           #
+    # Hint: Instead of computing the gradient from scratch, it may be easier    #
+    # to reuse some of the intermediate values that you used to compute the     #
+    # loss.                                                                     #
+    #############################################################################
+    # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    margins[margins > 0] = 1 # binarize the matrix
+    row_sum = np.sum(margins, axis=1) # D x 1
+    margins[np.arange(num_train), y] = -row_sum.T
+    
+    dW = np.dot(X.T, margins)
+    dW /= num_train # average 
+    dW += 2*reg*W # regularization
+
+    # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    return loss, dW
